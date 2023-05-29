aef4281b95fdf0eb42ce688ebb6e9c657e38e43f
primzl <123480849+primzl@users.noreply.github.com>
2023-05-29 10:37:28 +0200

    Update streamlit_app.py
    Error

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
-streamlit.header('Breakfast Menu')
-   🥑🍞
+streamlit.header('Breakfast Favorites')
+
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')

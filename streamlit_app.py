6cf8240abbccd77ca766b10d5dc47b86fc60501d
primzl <123480849+primzl@users.noreply.github.com>
2023-06-19 13:56:47 +0200

    Update streamlit_app.py
    Error

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,7 @@ try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error('Enter a fruit!')
+  else:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
